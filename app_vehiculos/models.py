6357bca95e884c5e9053545a1723880c1c17a40d
Juan Flores <sacheflores@gmail.com>
2023-11-14 00:28:03 -0300

    Modelos, actualizamos la BD

---
+++ app_vehiculos/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Camioneta(models.Model):
+    #atributos de clases
+    marca = models.CharField(max_length=256)
+    modelo = models.CharField(max_length=256)
+    fabricacion = models.IntegerField()
+    cv= models.IntegerField(blank=True)
+    peso = models.IntegerField()
+
+class Automovil(models.Model):
+    marca = models.CharField(max_length=256)
+    modelo = models.CharField(max_length=256)
+    fabricacion = models.IntegerField()
+    cv = models.IntegerField(blank=True)
+    peso = models.IntegerField()
+
+
+class Motocicleta(models.Model):
+    marca = models.CharField(max_length=256)
+    modelo = models.CharField(max_length=256)
+    fabricacion = models.IntegerField()
+    cv = models.IntegerField(blank=True)
+    peso = models.IntegerField()
