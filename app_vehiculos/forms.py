2fb52c04cf445378e27f5929052681bf30e063a6
Juan Flores <sacheflores@gmail.com>
2023-11-19 21:02:41 -0300

    nuevos formularios

--- app_vehiculos/forms.py
+++ app_vehiculos/forms.py
@@ -1,9 +1,22 @@
 from django import forms
 
-class VehiculosFormulario(forms.Form):
+class CamionetaFormulario(forms.Form):
     marca = forms.CharField(required=True, max_length=64)
-    modelo = forms.CharField(max_length=256)
-    comision = forms.IntegerField(required=True, max_value=80000)    
+    modelo = forms.CharField(required=True, max_length=256)
     fabricacion = forms.IntegerField(required=True, max_value=80000) 
-    cv= forms.IntegerField(required=True, max_value=80000) 
-    peso= forms.IntegerField(required=True, max_value=80000)
+    cv= forms.IntegerField(max_value=80000) 
+    peso= forms.IntegerField(max_value=80000)
+    
+class AutomovilFormulario(forms.Form):
+    marca = forms.CharField(required=True, max_length=64)
+    modelo = forms.CharField(required=True, max_length=256) 
+    fabricacion = forms.IntegerField(required=True, max_value=80000) 
+    cv= forms.IntegerField(max_value=80000) 
+    peso= forms.IntegerField(max_value=80000)
+    
+class MotocicletaFormulario(forms.Form):
+    marca = forms.CharField(required=True, max_length=64)
+    modelo = forms.CharField(required=True, max_length=256)  
+    fabricacion = forms.IntegerField(required=True, max_value=80000) 
+    cv= forms.IntegerField(max_value=80000) 
+    peso= forms.IntegerField(max_value=80000)
