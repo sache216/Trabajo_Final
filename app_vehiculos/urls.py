943f88b5c3d6fad35651ecc3f74f4906c127cedd
Juan Flores <sacheflores@gmail.com>
2023-11-20 19:36:02 -0300

    editar vehiculos

--- app_vehiculos/urls.py
+++ app_vehiculos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from app_vehiculos.views import (
  listar_camioneta, listar_automovil, listar_motocicleta, crear_camioneta, crear_automovil,
- crear_motocicleta, buscar_camionetas, buscar_automoviles, buscar_motocicletas, eliminar_motocicleta, eliminar_camioneta, eliminar_automovil
+ crear_motocicleta, buscar_camionetas, buscar_automoviles, buscar_motocicletas, eliminar_motocicleta,
+ eliminar_camioneta, eliminar_automovil, editar_camioneta, editar_automovil, editar_motocicleta
 )
 
 
@@ -9,15 +10,22 @@ urlpatterns = [
    path('camionetas/', listar_camioneta, name="listar_camionetas"),
    path('automoviles/', listar_automovil, name="listar_automoviles"),
    path('motocicletas/', listar_motocicleta, name="listar_motocicletas"),
+   
    path("crear-camioneta/", crear_camioneta, name="crear_camioneta"),
    path("crear-automovil/", crear_automovil, name="crear_automovil"),
    path("crear-motocicleta/", crear_motocicleta, name="crear_motocicleta"),
+   
    path("buscar-camionetas/", buscar_camionetas, name="buscar_camionetas"),
    path("buscar-autos/", buscar_automoviles, name="buscar_automoviles"),
    path("buscar-motos/", buscar_motocicletas, name="buscar_motocicletas"),
+   
    path('eliminar-motocicleta/<int:id>/', eliminar_motocicleta, name="eliminar_motocicleta"),
    path('eliminar-camioneta/<int:id>/', eliminar_camioneta, name="eliminar_camioneta"),
    path('eliminar-automovil/<int:id>/', eliminar_automovil, name="eliminar_automovil"),
+   
+   path('editar-camioneta/<int:id>/', editar_camioneta, name="editar_camioneta"),
+   path('editar-automovil/<int:id>/', editar_automovil, name="editar_automovil"),
+   path('editar-motocicleta/<int:id>/', editar_motocicleta, name="editar_motocicleta"),
 
 
 ]
--- app_vehiculos/views.py
+++ app_vehiculos/views.py
@@ -188,3 +188,118 @@ def eliminar_automovil(request, id): #borra sin confirmacion
        automovil.delete()
        url_exitosa = reverse('listar_automoviles')
        return redirect(url_exitosa)
+
+
+def editar_camioneta(request, id):
+    # Obtener la instancia de la camioneta con el ID proporcionado
+    camioneta = Camioneta.objects.get(id=id)
+
+    if request.method == "POST":
+        # Si la solicitud es de tipo POST, procesar el formulario
+        formulario = CamionetaFormulario(request.POST)
+
+        if formulario.is_valid():
+            # Si el formulario es válido, obtener los datos y actualizar la instancia de la camioneta
+            data = formulario.cleaned_data
+            camioneta.marca = data["marca"]
+            camioneta.modelo = data["modelo"]
+            camioneta.fabricacion = data["fabricacion"]
+            camioneta.cv = data["cv"]
+            camioneta.peso = data["peso"]
+            camioneta.save()
+
+            # Redirigir a la URL exitosa después de la edición
+            url_exitosa = reverse('listar_camionetas')
+            return redirect(url_exitosa)
+    else:  # Si la solicitud es GET, inicializar el formulario con los datos actuales de la camioneta
+        inicial = {
+            'marca': camioneta.marca,
+            'modelo': camioneta.modelo,
+            'fabricacion': camioneta.fabricacion,
+            'cv': camioneta.cv,
+            'peso': camioneta.peso,
+        }
+        formulario = CamionetaFormulario(initial=inicial)
+
+    # Renderizar el formulario para la edición
+    return render(
+        request=request,
+        template_name='app_vehiculos/formulario_camioneta.html',
+        context={'formulario': formulario},
+    )
+    
+def editar_automovil(request, id):
+    # Obtener la instancia de la automovil con el ID proporcionado
+    automovil = Automovil.objects.get(id=id)
+
+    if request.method == "POST":
+        # Si la solicitud es de tipo POST, procesar el formulario
+        formulario = AutomovilFormulario(request.POST)
+
+        if formulario.is_valid():
+            # Si el formulario es válido, obtener los datos y actualizar la instancia de la automovil
+            data = formulario.cleaned_data
+            automovil.marca = data["marca"]
+            automovil.modelo = data["modelo"]
+            automovil.fabricacion = data["fabricacion"]
+            automovil.cv = data["cv"]
+            automovil.peso = data["peso"]
+            automovil.save()
+
+            # Redirigir a la URL exitosa después de la edición
+            url_exitosa = reverse('listar_automoviles')
+            return redirect(url_exitosa)
+    else:  # Si la solicitud es GET, inicializar el formulario con los datos actuales de la automovil
+        inicial = {
+            'marca': automovil.marca,
+            'modelo': automovil.modelo,
+            'fabricacion': automovil.fabricacion,
+            'cv': automovil.cv,
+            'peso': automovil.peso,
+        }
+        formulario = AutomovilFormulario(initial=inicial)
+
+    # Renderizar el formulario para la edición
+    return render(
+        request=request,
+        template_name='app_vehiculos/formulario_automovil.html',
+        context={'formulario': formulario},
+    )
+    
+def editar_motocicleta(request, id):
+    # Obtener la instancia de la motocicleta con el ID proporcionado
+    motocicleta = Motocicleta.objects.get(id=id)
+
+    if request.method == "POST":
+        # Si la solicitud es de tipo POST, procesar el formulario
+        formulario = MotocicletaFormulario(request.POST)
+
+        if formulario.is_valid():
+            # Si el formulario es válido, obtener los datos y actualizar la instancia de la motocicleta
+            data = formulario.cleaned_data
+            motocicleta.marca = data["marca"]
+            motocicleta.modelo = data["modelo"]
+            motocicleta.fabricacion = data["fabricacion"]
+            motocicleta.cv = data["cv"]
+            motocicleta.peso = data["peso"]
+            motocicleta.save()
+
+            # Redirigir a la URL exitosa después de la edición
+            url_exitosa = reverse('listar_motocicletas')
+            return redirect(url_exitosa)
+    else:  # Si la solicitud es GET, inicializar el formulario con los datos actuales de la motocicleta
+        inicial = {
+            'marca': motocicleta.marca,
+            'modelo': motocicleta.modelo,
+            'fabricacion': motocicleta.fabricacion,
+            'cv': motocicleta.cv,
+            'peso': motocicleta.peso,
+        }
+        formulario = MotocicletaFormulario(initial=inicial)
+
+    # Renderizar el formulario para la edición
+    return render(
+        request=request,
+        template_name='app_vehiculos/formulario_motocicleta.html',
+        context={'formulario': formulario},
+    )
