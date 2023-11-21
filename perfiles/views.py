94d0c4a3f7301d73d84e65a6052ffb0c4fb6f17f
Juan Flores <sacheflores@gmail.com>
2023-11-21 18:46:17 -0300

    login

--- perfiles/views.py
+++ perfiles/views.py
@@ -25,3 +25,27 @@ def registro(request):
         template_name='perfiles/registro.html',
         context={'form': formulario},
     )
+    
+    
+def login_view(request):
+   if request.method == "POST":
+       form = AuthenticationForm(request, data=request.POST)
+
+       if form.is_valid():
+           data = form.cleaned_data
+           usuario = data.get('username')
+           password = data.get('password')
+           user = authenticate(username=usuario, password=password)
+           # user puede ser un usuario o None
+           if user:
+               login(request=request, user=user)
+               url_exitosa = reverse('inicio')
+               return redirect(url_exitosa)
+   else:  # GET
+       form = AuthenticationForm()
+   return render(
+       request=request,
+       template_name='perfiles/login.html',
+       context={'form': form},
+   )
+
