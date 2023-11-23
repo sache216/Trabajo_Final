acf01faabedb62e412dfc71a402a7b28e8b79f72
Juan Flores <sacheflores@gmail.com>
2023-11-23 19:51:20 -0300

    fin del UnitTest

--- app_vehiculos/tests.py
+++ app_vehiculos/tests.py
@@ -17,15 +17,15 @@ class CamionetaTests(TestCase):
     def test_camioneta_str(self):
         camioneta = Camioneta(marca="Volkswagen", modelo="Amarok", fabricacion=2019, cv=258, peso=2820)
         camioneta.save()
-
-    def __str__(self):
-        return f"{self.marca} {self.modelo} ({self.fabricacion})({self.cv})({self.peso})"
+        
+        
+        self.assertEqual(camioneta.__str__(), "Volkswagen Amarok (2019)(258)(2820)")
     
 
 class AutomovilTests(TestCase):
 
     def test_creacion_automovil(self):
-        automovil = Automovil(marca="Fiat", modelo="uno", fabricacion=2000, cv=150, peso=800)
+        automovil = Automovil(marca="Fiat", modelo="uno", fabricacion=2000, cv=150, peso=1000)
         automovil.save()
 
         self.assertEqual(Automovil.objects.count(), 1)
@@ -33,15 +33,32 @@ class AutomovilTests(TestCase):
         self.assertEqual(automovil.modelo, "uno")
         self.assertEqual(automovil.fabricacion, 2000)
         self.assertEqual(automovil.cv, 150)
-        self.assertEqual(automovil.peso, 800)
+        self.assertEqual(automovil.peso, 1000)
 
     def test_automovil_str(self):
-        automovil = Automovil(marca="Fiat", modelo="uno", fabricacion=2000, cv=150, peso=800)
+        automovil = Automovil(marca="Fiat", modelo="uno", fabricacion=2000, cv=150, peso=1000)
         automovil.save()
 
-        self.assertEqual(automovil.__str__(), "Fiat uno (2000)(150)(800)")
+        self.assertEqual(automovil.__str__(), "Fiat uno (2000)(150)(1000)")
 
 
+class MotocicletaTests(TestCase):
 
+    def test_creacion_motocicleta(self):
+        motocicleta = Motocicleta(marca="Honda", modelo="Tornado", fabricacion=2018, cv=94, peso=800)
+        motocicleta.save()
+
+        self.assertEqual(Motocicleta.objects.count(), 1)
+        self.assertEqual(motocicleta.marca, "Honda")
+        self.assertEqual(motocicleta.modelo, "Tornado")
+        self.assertEqual(motocicleta.fabricacion, 2018)
+        self.assertEqual(motocicleta.cv, 94)
+        self.assertEqual(motocicleta.peso, 800)
+
+    def test_motocicleta_str(self):
+        motocicleta = Motocicleta(marca="Honda", modelo="tornado", fabricacion=2018, cv=94, peso=800)
+        motocicleta.save()
+        
+        self.assertEqual(motocicleta.__str__(), "Honda tornado (2018)(94)(800)")
 
 
